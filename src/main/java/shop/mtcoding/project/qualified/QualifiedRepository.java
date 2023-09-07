558e304ebdec1c8802a83ebe71578c6b954e7826
muaga <more9494@naver.com>
2023-09-07 10:52:46 +0900

    추가 합치기

--- src/main/java/shop/mtcoding/project/qualified/QualifiedRepository.java
+++ src/main/java/shop/mtcoding/project/qualified/QualifiedRepository.java
@@ -3,6 +3,7 @@ package shop.mtcoding.project.qualified;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -15,4 +16,8 @@ public interface QualifiedRepository extends JpaRepository<Qualified, Integer> {
 
     @Query("select q from Qualified q where q.jobOpening.id = :id")
     List<Qualified> mfindByJobOpeningId(@Param("id") Integer id);
+
+    @Modifying
+    @Query("delete from Qualified q where q.jobOpening.id = :id")
+    void deleteByJobOpeningId(@Param("id") Integer id);
 }
--- src/main/java/shop/mtcoding/project/task/TaskRepository.java
+++ src/main/java/shop/mtcoding/project/task/TaskRepository.java
@@ -1,19 +1,23 @@
 package shop.mtcoding.project.task;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
     @Query("select t from Task t where t.taskContent = :taskContent")
-    Optional<Task> findByTaskName(@Param("taskContent") String taskContent);
+    Task findByTaskName(@Param("taskContent") String taskContent);
 
     @Query("select t from Task t where t.jobOpening.id = :jobOpeningId")
     List<Task> findByJobOpeningId(@Param("jobOpeningId") Integer jobOpeningId);
 
     @Query("select t from Task t where t.jobOpening.id = :id")
     List<Task> mfindByJobOpeningId(@Param("id") Integer id);
+
+    @Modifying
+    @Query("delete from Task t where t.jobOpening.id = :id")
+    void deleteByJobOpeningId(@Param("id") Integer id);
 }
